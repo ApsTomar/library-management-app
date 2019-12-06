642c5ac78828281f1b0dc8ca49fc191d83cbc2d9
Abhay.Tomar <abhaypratap305@gmail.com>
2019-12-06 19:17:30 +0530

    book detail component

---
+++ src/app/book-detail/book-detail.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Book } from '../models';
+import { BookService } from '../book.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-book-detail',
+  templateUrl: './book-detail.component.html',
+  styleUrls: ['./book-detail.component.css']
+})
+export class BookDetailComponent implements OnInit {
+  @Input() book: Book
+  constructor(
+    private bookService: BookService,
+    private location: Location,
+    private activatedRoute: ActivatedRoute,
+  ) { }
+
+  ngOnInit() {
+    this.getBook();
+  }
+
+  getBook(): void {
+    const id = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.bookService.getBook(id)
+      .subscribe(book => this.book = book);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+}
