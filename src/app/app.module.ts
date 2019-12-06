03209a087664d17ad6e8e9d5b772f40d51d8fdce
Abhay.Tomar <abhaypratap305@gmail.com>
2019-12-06 19:16:27 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BooksComponent,
+    BookDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
